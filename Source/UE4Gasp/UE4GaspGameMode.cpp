576915abdc9862398566f195794c5d57d362849b
Mickael Daniel <daniel.mickael@gmail.com>
2018-12-19 15:44:00 +0100

    Cleanup / Reorganize project folders

--- Source/UE4Gasp/UE4GaspGameMode.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
-
-#include "UE4GaspGameMode.h"
-
-AUE4GaspGameMode::AUE4GaspGameMode()
-	: Super()
-{
-	UE_LOG(LogTemp, Warning, TEXT("Initializing Game Mode"));
-}
